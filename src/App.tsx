592e41bc9aa837548f10917207b2541d5fcd82b0
Saeed <hamidradpj@gmail.com>
2024-02-05 21:34:41 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import {
+  Abstraxion,
+  useAbstraxionAccount,
+  useAbstraxionSigningClient,
+} from "@burnt-labs/abstraxion";
+import { Button } from "@burnt-labs/ui";
+import "@burnt-labs/ui/styles.css";
+import type { ExecuteResult } from "@cosmjs/cosmwasm-stargate";
+
+import { seatContractAddress } from "./Const.tsx";
+import "./App.css";
+
+type ExecuteResultOrUndefined = ExecuteResult | undefined;
+
+function App() {
+  // Abstraxion hooks
+  const { data: account } = useAbstraxionAccount();
+  const { client } = useAbstraxionSigningClient();
+
+  // General state hooks
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [executeResult, setExecuteResult] =
+    useState<ExecuteResultOrUndefined>(undefined);
+
+  const blockExplorerUrl = `https://explorer.burnt.com/xion-testnet-1/tx/${executeResult?.transactionHash}`;
+
+  function getTimestampInSeconds(date: Date | null) {
+    if (!date) return 0;
+    const d = new Date(date);
+    return Math.floor(d.getTime() / 1000);
+  }
+
+  const now = new Date();
+  now.setSeconds(now.getSeconds() + 15);
+  const oneYearFromNow = new Date();
+  oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+
+  async function claimSeat() {
+    setLoading(true);
+    const msg = {
+      sales: {
+        claim_item: {
+          token_id: String(getTimestampInSeconds(now)),
+          owner: account.bech32Address,
+          token_uri: "",
+          extension: {},
+        },
+      },
+    };
+
+    try {
+      const claimRes = await client?.execute(
+        account.bech32Address,
+        seatContractAddress,
+        msg,
+        {
+          amount: [{ amount: "0", denom: "uxion" }],
+          gas: "500000",
+        },
+        "",
+        []
+      );
+
+      setExecuteResult(claimRes);
+    } catch (error) {
+      // eslint-disable-next-line no-console -- No UI exists yet to display errors
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <main className="m-auto flex min-h-screen max-w-xs flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-2xl font-bold tracking-tighter text-white">
+        ABSTRAXION
+      </h1>
+      <Button
+        fullWidth
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        structure="base"
+      >
+        {account.bech32Address ? (
+          <div className="flex items-center justify-center">VIEW ACCOUNT</div>
+        ) : (
+          "CONNECT"
+        )}
+      </Button>
+      {client ? (
+        <Button
+          disabled={loading}
+          fullWidth
+          onClick={() => {
+            void claimSeat();
+          }}
+          structure="base"
+        >
+          {loading ? "LOADING..." : "CLAIM SEAT"}
+        </Button>
+      ) : null}
+      <Abstraxion
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      />
+      {executeResult ? (
+        <div className="flex flex-col rounded border-2 border-black p-2 dark:border-white">
+          <div className="mt-2">
+            <p className="text-zinc-500">
+              <span className="font-bold">Transaction Hash</span>
+            </p>
+            <p className="text-sm">{executeResult.transactionHash}</p>
+          </div>
+          <div className="mt-2">
+            <p className=" text-zinc-500">
+              <span className="font-bold">Block Height:</span>
+            </p>
+            <p className="text-sm">{executeResult.height}</p>
+          </div>
+          <div className="mt-2">
+            <a
+              className="text-black underline visited:text-purple-600 dark:text-white"
+              href={blockExplorerUrl}
+              target="_blank"
+            >
+              View in Block Explorer
+            </a>
+          </div>
+        </div>
+      ) : null}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Const.tsx
@@ -0,0 +1,2 @@
+export const seatContractAddress =
+  "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka";
