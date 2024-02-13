84dad406602c9312fcd05df85c9ec5a75b4be6fd
sam <meisamtaher71@gmail.com>
2024-02-14 03:13:06 +0330

    change configs

--- src/Const.tsx
+++ src/Const.tsx
@@ -1,7 +1,7 @@
 export const HPCAddress =
   "xion1rd9a9rdw6gued7hf079a7gngyk0frnlqcc94y8xnhnkydsjzzzvsmxk5fk";
 export const HaypayAddress = 
-"xion152yggskpkke4wz6jaqv4awupzgl868pfxusdf8r9y6gjyngctkjqtz5m9q";
+"xion1wxp3q98za6w9a2g5t2hvrdlh5lvasv0f3p085m0rquk9u4c8xeeqvyrnx6";
 interface token{
   token_address:string,
   logo:string,
